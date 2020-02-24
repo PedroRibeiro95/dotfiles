ac9a1239167c0b68eefcaf8de7733657c992d238
Pedro Ribeiro <pedro.ribeiro.pr.95@gmail.com>
2020-02-24 16:23:42 +0000

    Update dotfiles

--- .config/polybar/i3wmthemer_bar_launch.sh
+++ .config/polybar/i3wmthemer_bar_launch.sh
@@ -4,4 +4,10 @@ pkill polybar
 
 sleep 1;
 
-polybar i3wmthemer_bar &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload i3wmthemer_bar &
+  done
+else
+  polybar --reload i3wmthemer_bar &
+fi
